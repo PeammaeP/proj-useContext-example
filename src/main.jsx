d7aac0d45abddc88c8fa585a9844870fad46d973
PeamMiiZ <peemmygg@gmail.com>
2024-10-20 12:00:43 +0700

    add feature provides the components with pages and add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,20 @@
+const UserProfile = () => {
+  const user = {
+    email: "test@mail.com",
+    job: "Developer",
+  };
+
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h2 className="text-2xl font-bold mb-4">ข้อมูลผู้ใช้</h2>
+      <p>
+        <strong>อีเมล:</strong> {user.email}
+      </p>
+      <p>
+        <strong>อาชีพ:</strong> {user.job}
+      </p>
+    </div>
+  );
+};
+
+export default UserProfile;
